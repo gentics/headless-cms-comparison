4dbc10b6791958eb48410d054c4f27443d1ac1ec
Gortan Philipp <p.gortan@gentics.com>
2020-09-02 12:55:44 +0200

    add SPA production build to test GitHub Pages

---
+++ spa/build/precache-manifest.55043fef0776f9a0aad94a46cdbf1c10.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7cf3c356e152fdeb8d69ff559f60c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0945a8ec4481452c669f",
+    "url": "/static/css/2.3d9100e7.chunk.css"
+  },
+  {
+    "revision": "d48cefa826d272f09f58",
+    "url": "/static/css/main.4a7aed5c.chunk.css"
+  },
+  {
+    "revision": "0945a8ec4481452c669f",
+    "url": "/static/js/2.d6f5b792.chunk.js"
+  },
+  {
+    "revision": "e59e05b433162196f621becd990ea15d",
+    "url": "/static/js/2.d6f5b792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48cefa826d272f09f58",
+    "url": "/static/js/main.c0659fc2.chunk.js"
+  },
+  {
+    "revision": "37ba70f12ad78ba761ef",
+    "url": "/static/js/runtime-main.52788ced.js"
+  },
+  {
+    "revision": "4fc02b15a885f58ba3695783abda8376",
+    "url": "/static/media/background.4fc02b15.svg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/static/media/primeicons.79432094.woff"
+  }
+]);
